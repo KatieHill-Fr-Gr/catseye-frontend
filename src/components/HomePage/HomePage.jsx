c9fc7e2d816961e3e7db4b9b6d52e502fa34137d
Katie Hill <khill.fr.gr@gmail.com>
2025-08-31 18:04:01 +0100

    Homepage layout adjusted

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,26 +5,27 @@ import { Link } from 'react-router-dom'
 
 
 const HomePage = () => {
-  const navigate = useNavigate()
+    const navigate = useNavigate()
 
-  return (
-    <main>
-    <section className="hero">
-      <h1>Collaborate, Translate, Manage WorkFlows</h1>
-      <p>Keep all your multilingual content in one place.</p>
-            <button className="page-button">Create a project</button>
-    </section>
-    <section className="page-content">
-      <div id="categories">
-        <h2>Categories</h2>
-      <p>Browse the latest items for sale in each category.</p>
-      <button className="page-button">Discover all</button>
-      <div className="image-row">
-      </div>
-      </div>
-      </section>
-    </main>
-  )
+    return (
+        <main>
+            <section className="hero">
+                <h1>Collaborate, Translate, Manage WorkFlows</h1>
+                <p>Keep all your multilingual content in one place.</p>
+                <button className="page-button">Create a project</button>
+            </section>
+            <section className="page-content">
+                <div class="content-wrapper">
+                    <div class="text-column">
+                        <h2>Streamlined project management</h2>
+                        <div></div>
+                    </div>
+                </div>
+                <div class="image-column">
+                    </div>
+            </section>
+        </main>
+    )
 }
 
 export default HomePage
