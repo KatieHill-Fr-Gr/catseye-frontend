5f624bee6d551ad6b7067b512d06d1275a104a12
Katie Hill <khill.fr.gr@gmail.com>
2025-09-01 22:54:59 +0100

    404 page layout fixed

--- src/components/404NotFound/404NotFound.jsx
+++ src/components/404NotFound/404NotFound.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
     return (
-        <div className="page-content">
+        <div className="hero">
             <div className="notfound-image">
             <img src={eyeIcon} alt="White silhouette of eye" />
             </div>
             <h1> Oops, page not found</h1>
             <p>We can't find the page you were looking for.</p>
-            <Link to="/items" className="page-button">Continue shopping</Link>
+            <Link to="/projects" className="page-button">Go back to projects</Link>
         </div>
     )
 
